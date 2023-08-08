8f5af7764cc099df6a387abfc4c71a78de0e6734
KaysarovMovsar <kaysarovmovsarch@mail.ru>
2023-08-08 14:44:57 +0300

    lesson_9

--- src/action.js
+++ src/action.js
@@ -1,3 +1,5 @@
+import {jsx} from "react/jsx-runtime";
+
 export const loadTodos = () => {
     return (dispatch) => {
         dispatch({ type: "load/todos/start"})
@@ -8,6 +10,21 @@ export const loadTodos = () => {
                 dispatch({
                     type: "load/todos/fulfilled",
                     payload: json
+                })
+            })
+    }
+}
+
+export const RemoveTodo = (id) => {
+    return (dispatch) => {
+        dispatch({type: "delete/todo/start"})
+
+        fetch(`https://jsonplaceholder.typicode.com/users/${id}`, {method: "DELETE"})
+            .then((response) => response.json())
+            .then((json) => {
+                dispatch({
+                    type: "delete/todo/fulfilled",
+                    payload: id
                 })
             })
     }
